eaa327e6b2a5bf52d612905a6f71340b8bbcdac6
divyameher <divyaramesh1594@gmail.com>
2021-05-28 17:21:55 +0530

    feat(star-rating):to display stars based on rating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { EmployeeCardComponent } from './shared/employee-card/employee-card.component';
+import { StarRatingComponent } from './shared/star-rating/star-rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    EmployeeCardComponent
+    EmployeeCardComponent,
+    StarRatingComponent
   ],
   imports: [
     BrowserModule,
