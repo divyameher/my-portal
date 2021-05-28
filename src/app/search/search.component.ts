dad1730a47265441bd03bdc5a4bc1e16f95ea002
divyameher <divyaramesh1594@gmail.com>
2021-05-28 18:03:42 +0530

    feat(search):employees data binding

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,10 +6,84 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
+  employees: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.employees = [
+      {
+        profilePic: '../../assets/images/employees/chandler_bing.PNG',
+        name: 'Chandler Bing',
+        designation: 'Sr.UI Developer',
+        ratings: 3.5,
+        experience: 5.8,
+        dateOfJoining: 2017,
+        team: 'OCBC Singapore',
+        manager: 'Lalit Agarawal',
+        mobileNo: '7406559241',
+        email: 'cbing@infrrd.ai'
+      },
+      {
+        profilePic: '../../assets/images/employees/monica_geller.PNG',
+        name: 'Monica Geller',
+        designation: 'Sr.UI Developer',
+        ratings: 4,
+        experience: 4,
+        dateOfJoining: 2018,
+        team: 'OCBC Singapore',
+        manager: 'Lalit Agarawal',
+        mobileNo: '7406559241',
+        email: 'mgeller@infrrd.ai'
+      },
+      {
+        profilePic: '../../assets/images/employees/phoebe_buffay.PNG',
+        name: 'Phoebe Buffay',
+        designation: 'Sr.UI Developer',
+        ratings: 5,
+        experience: 2,
+        dateOfJoining: 2019,
+        team: 'OCBC Singapore',
+        manager: 'Lalit Agarawal',
+        mobileNo: '7406588841',
+        email: 'pbuffay@infrrd.ai'
+      },
+      {
+        profilePic: '../../assets/images/employees/rachel_green.PNG',
+        name: 'Rachel Green',
+        designation: 'Sr.UI Developer',
+        ratings: 3,
+        experience: 2,
+        dateOfJoining: 2019,
+        team: 'OCBC Singapore',
+        manager: 'Lalit Agarawal',
+        mobileNo: '7406599941',
+        email: 'rgreen@infrrd.ai'
+      },
+      {
+        profilePic: '../../assets/images/employees/ross_geller.PNG',
+        name: 'Ross Geller',
+        designation: 'Sr.UI Developer',
+        ratings: 4,
+        experience: 5,
+        dateOfJoining: 2019,
+        team: 'OCBC Singapore',
+        manager: 'Lalit Agarawal',
+        mobileNo: '7406449941',
+        email: 'rgeller@infrrd.ai'
+      },
+      {
+        profilePic: '../../assets/images/employees/sheldon_cooper.PNG',
+        name: 'Sheldon Cooper',
+        designation: 'Sr.UI Developer',
+        ratings: 5,
+        experience: 7,
+        dateOfJoining: 2009,
+        team: 'OCBC Singapore',
+        manager: 'Lalit Agarawal',
+        mobileNo: '7555449941',
+        email: 'scooper@infrrd.ai'
+      }
+    ]
   }
 
 }
